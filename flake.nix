70ca5fc5d6b5fd72c42caf20a3e3d527f675f64e
dayvidpham <dayvidpham@gmail.com>
2023-12-24 15:28:20 -0800

    Migrate to using buildNpmPackage to package Node dependencies

--- flake.nix
+++ flake.nix
@@ -13,57 +13,23 @@
         buildInputs = [
           pkgs.nodejs_20
         ];
-
-        # Read the package-lock.json as a Nix attrset
-        packageLock = builtins.fromJSON (builtins.readFile (./. + "/package-lock.json"));
-
-        # Create an array of all (meaningful) dependencies
-        deps = builtins.attrValues (removeAttrs packageLock.packages [ "" ])
-          # ++ builtins.attrValues (removeAttrs (packageLock.dependencies) [ "" ])
-        ;
-
-        # Turn each dependency into a fetchurl call
-        tarballs = map (p: pkgs.fetchurl { url = p.resolved; hash = p.integrity; }) deps;
-
-        # Write a file with the list of tarballs
-        tarballsFile = pkgs.writeTextFile {
-          name = "tarballs";
-          text = builtins.concatStringsSep "\n" tarballs;
-        }; 
-
     in {
         devShells.default = pkgs.mkShell {
             inherit buildInputs;
             shellHook = ''
               echo "node `node --version`"
               echo "npm `npm --version`"
-              # echo "pnpm `pnpm --version`"
             '';
         };
 
-        packages.default = pkgs.stdenv.mkDerivation {
-            inherit (packageLock) name version;
-            inherit buildInputs;
+        packages.default = pkgs.buildNpmPackage {
+            pname = "dhpham-website";
+            version = "0.0.1";
             src     = ./.;
+            npmDepsHash = "sha256-T/sR+XTIF+R0UAfFqsmoy//dvrv2KOV7HR1fiQ327k8=";
 
-            buildPhase = ''
-              export HOME=$PWD/.home
-              export npm_config_cache=$PWD/.npm
-              mkdir -p $out/js
-              cd $out/js
-              cp -r $src/. .
-              
-              while read package
-              do
-                echo "caching $package"
-                npm cache add "$package"
-              done <${tarballsFile}
-
-              npm ci
-            '';
-            installPhase = ''
-              ln -s $out/js/node_modules/.bin $out/bin
-            ''; 
+            inherit buildInputs;
+            npmPackFlags = [ "--ignore-scripts" ];
         };
     });
 }
