adb8d4cc0e54f8e0120dcee6984ea83b3221f7c6
dayvidpham <dayvidpham@gmail.com>
2024-01-03 15:52:50 -0800

    Add point that has periodic Y motion while moving across the screen

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,49 @@ window.addEventListener('resize', handleResize(canvas));
 
 const ctx = canvas.getContext("2d");
 if (ctx == null) {
-    throw new ReferenceError('Canvas cannot return 2d context');
+    throw new ReferenceError('Canvas failed to return 2d context');
 }
 
-ctx.fillStyle = '#da5225';
-ctx.beginPath();
-ctx.arc(canvas.width/2, canvas.height/2, 200, 0, 2*Math.PI);
-ctx.closePath();
-ctx.fill();
+function animateFromContext(ctx: CanvasRenderingContext2D) {
+    let previousTimestamp = 0;
+    let radius = 200;
+    let x = 0,
+        y = 0,
+        yy = 0;
+
+    const xSpeed    = 500 / 1000;           // px/ms
+    const yDispl    = 200;                  // px/ms
+    const yPeriod   = 2*Math.PI / 2000;     // radians/ms, one full period per 2 secs
+
+    const animate = function periodic(timestamp: number) {
+        if(previousTimestamp === 0) {
+            previousTimestamp = timestamp;
+        }
+
+        const update = timestamp - previousTimestamp;
+
+        x += update*xSpeed;
+        if(x > ctx.canvas.width + radius) {
+            x = -radius
+        }
+
+        yy += update*yPeriod;
+        y = yDispl*Math.sin(yy);
+        console.log(y);
+
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        ctx.fillStyle = '#da5225';
+        ctx.beginPath();
+        ctx.arc(x, y+ctx.canvas.height/2, 200, 0, 2*Math.PI);
+        ctx.closePath();
+        ctx.fill();
+
+        previousTimestamp = timestamp;
+        window.requestAnimationFrame(animate);
+    }
+    return animate;
+};
+
+window.requestAnimationFrame(animateFromContext(ctx));
+
