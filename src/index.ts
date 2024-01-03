4a47ac44c29b89bd0188436c3ba078d23d28e743
dayvidpham <dayvidpham@gmail.com>
2024-01-02 18:35:28 -0800

    Serves MuseoModerno from local

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 const hello: String = "Hello, World";
-console.log(hello);
+const canvas: HTMLCanvasElement | null = document.querySelector("#main-canvas");
+const ctx = canvas?.getContext("2d");
+console.log(ctx);
