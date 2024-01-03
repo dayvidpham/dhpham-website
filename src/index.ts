09f6da8e7480cbc431a463f811445e1e4c262c2a
dayvidpham <dayvidpham@gmail.com>
2024-01-02 20:23:02 -0800

    Fix canvas to size of screen and draw circle in center
    * TODO: redraw or resize

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
-const hello: String = "Hello, World";
-const canvas: HTMLCanvasElement | null = document.querySelector("#main-canvas");
-const ctx = canvas?.getContext("2d");
-console.log(ctx);
+function handleResize (canvas: HTMLCanvasElement) {
+    return function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    };
+};
+
+// Init canvas
+const canvasQuery: HTMLCanvasElement | null = document.querySelector("#main-canvas");
+if (canvasQuery == null) {
+    throw new ReferenceError('Could not find #main-canvas element: something is horribly wrong. Exiting ...');
+}
+const canvas: HTMLCanvasElement = canvasQuery;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+window.addEventListener('resize', handleResize(canvas));
+
+const ctx = canvas.getContext("2d");
+if (canvas == null) {
+    throw new ReferenceError('Canvas cannot return 2d context');
+}
+
+ctx?.beginPath();
+ctx?.arc(canvas.width/2, canvas.height/2, 200, 0, 2*Math.PI);
+ctx?.closePath();
+ctx?.fill();
