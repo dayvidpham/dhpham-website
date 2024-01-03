789918eefe979f5a89317c3247d26da94a90e6ec
dayvidpham <dayvidpham@gmail.com>
2024-01-02 22:50:25 -0800

    Add more things todo

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,12 @@ canvas.height = window.innerHeight;
 window.addEventListener('resize', handleResize(canvas));
 
 const ctx = canvas.getContext("2d");
-if (canvas == null) {
+if (ctx == null) {
     throw new ReferenceError('Canvas cannot return 2d context');
 }
 
-ctx?.beginPath();
-ctx?.arc(canvas.width/2, canvas.height/2, 200, 0, 2*Math.PI);
-ctx?.closePath();
-ctx?.fill();
+ctx.fillStyle = '#da5225';
+ctx.beginPath();
+ctx.arc(canvas.width/2, canvas.height/2, 200, 0, 2*Math.PI);
+ctx.closePath();
+ctx.fill();
