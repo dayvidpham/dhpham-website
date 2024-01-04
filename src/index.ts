d12a46421be4a12006f1d44379f47691fb7522f8
dayvidpham <dayvidpham@gmail.com>
2024-01-03 23:24:44 -0800

    Add moving translating wave created with lines

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,109 @@
+// import Wave from './Wave';
+// maybe fuck the import: all in one file
+// easier to keep track of
+
+function lerp(x0: number, x1: number, t: number) {
+    return x0+(x1-x0)*t
+}
+
+function getRandomBetween(x0: number, x1: number) {
+    const low = Math.min(x0, x1);
+    const high = Math.max(x0, x1);
+    return Math.random()*(high-low) + low;
+}
+
+type Point2D = {
+    x: number;
+    y: number;
+};
+type WaveProps = {
+    ctx:        CanvasRenderingContext2D;
+    start:      Point2D;
+    end:        Point2D;
+    yPeriod:    number;
+    xlinspace:  number;
+}
+class Wave {
+    // Explicit
+    readonly ctx:       CanvasRenderingContext2D;
+    readonly start:     Point2D;
+    readonly end:       Point2D;
+    readonly yPeriod:   number;
+    readonly xlinspace: number;
+    // Implicit or set later
+    ySin:               number;
+    readonly nPoints:   number;
+    readonly tStep:     number;
+    prevTimeMs:         number;
+    frameId:            number | null;
+
+    constructor(props: WaveProps) {
+        this.ctx        = props.ctx;
+        this.start      = props.start;
+        this.end        = props.end;
+        this.yPeriod    = props.yPeriod;
+        this.xlinspace  = props.xlinspace;
+        // Implicit
+        this.ySin       = 0;
+        this.nPoints    = Math.floor((this.end.x - this.start.x) / this.xlinspace);
+        this.tStep      = 1 / this.nPoints;
+        this.prevTimeMs = -1;
+        this.frameId    = null;
+        // `this` will be undefined when re-called
+        this.render = this.render.bind(this);
+        this.update = this.update.bind(this);
+    }
+
+    update(timeMs: number) {
+        if (this.prevTimeMs === -1) {
+            this.prevTimeMs = timeMs;
+        }
+
+        const elapsed =  timeMs - this.prevTimeMs;
+        let x = this.start.x;
+
+        let y = this.start.y;
+        this.ySin += elapsed * this.yPeriod;
+        const yMagnitude = 100;
+
+        this.ctx.beginPath();
+        for(let t = 0; t <= 1; t += this.tStep) {
+            x = lerp(this.start.x, this.end.x, t) + getRandomBetween(-10, 10);
+            y = lerp(this.start.y, this.end.y, t) 
+                + Math.sin(t*4*Math.PI + this.ySin)*yMagnitude 
+                + getRandomBetween(-20, 20);
+
+            /////////////////////
+            // STROKE
+            // this.ctx.beginPath();
+            this.ctx.strokeStyle = "#fff";
+            this.ctx.lineWidth = 0.75;
+            this.ctx.lineTo(x, y);
+
+            /////////////////////
+            // FILL
+            // this.ctx.fillStyle = "#fff";
+            // this.ctx.arc(x, y, 10, 0, 2*Math.PI);
+            // this.ctx.fill();
+        }
+
+        this.ctx.stroke();
+
+        this.prevTimeMs = timeMs;
+        this.frameId = window.requestAnimationFrame(this.update);
+    }
+
+    draw(x: number, y: number) {
+        this.ctx.strokeStyle = "#fff";
+        this.ctx.lineWidth = 4;
+        this.ctx.lineTo(x, y);
+    }
+
+    render() {
+        window.requestAnimationFrame(this.update);
+    }
+}
+
 function handleResize (canvas: HTMLCanvasElement) {
     return function () {
         canvas.width = window.innerWidth;
@@ -20,6 +126,14 @@ if (ctx == null) {
     throw new ReferenceError('Canvas failed to return 2d context');
 }
 
+const wave = new Wave({
+    ctx: ctx,
+    start:      { x: 0, y: ctx.canvas.height / 2 },
+    end:        { x: ctx.canvas.width, y: ctx.canvas.height / 2 },
+    yPeriod:    2*Math.PI / 2000,
+    xlinspace:  10,
+});
+
 function animateFromContext(ctx: CanvasRenderingContext2D) {
     let previousTimestamp = 0;
     let radius = 200;
@@ -31,7 +145,7 @@ function animateFromContext(ctx: CanvasRenderingContext2D) {
     const yDispl    = 200;                  // px/ms
     const yPeriod   = 2*Math.PI / 2000;     // radians/ms, one full period per 2 secs
 
-    const animate = function periodic(timestamp: number) {
+    const render = function periodic(timestamp: number) {
         if(previousTimestamp === 0) {
             previousTimestamp = timestamp;
         }
@@ -45,7 +159,6 @@ function animateFromContext(ctx: CanvasRenderingContext2D) {
 
         yy += update*yPeriod;
         y = yDispl*Math.sin(yy);
-        console.log(y);
 
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
@@ -56,10 +169,11 @@ function animateFromContext(ctx: CanvasRenderingContext2D) {
         ctx.fill();
 
         previousTimestamp = timestamp;
-        window.requestAnimationFrame(animate);
+        window.requestAnimationFrame(render);
     }
-    return animate;
+    return render;
 };
 
 window.requestAnimationFrame(animateFromContext(ctx));
+wave.render();
 
